257f406e0ed635243794cf791dbd4515be6161b1
Jakob Sachs <jakobsachs1999@gmail.com>
2021-10-03 23:16:36 +0200

    Got basic colo printing down, but still alot of bugs, especially with special devices

--- src/ls.c
+++ src/ls.c
@@ -13,8 +13,12 @@
  *				03.10.2021
  *
  ******************************************************************************/
+#define COLOR_CODE_LENGTH 30
 #define INIT_BUFF_LEN 100
 #define MAX_LIST_SIZE 250
+
+// Catgories are:  dir, link, socket, pipe, exec, block, char
+#define CATEGORIES 7
 
 #include <stdio.h>
 #include <stdlib.h>
@@ -22,28 +26,38 @@
 #include <string.h>
 #include <dirent.h>
 #include <linux/limits.h>
+#include <sys/stat.h>
+
+#include "colors.h"
 
 struct prog_options
 {
   char directory[PATH_MAX];
-  int order_rev, show_hidden;
+  int order_rev, show_hidden, print_color;
 };
+
+typedef struct
+{
+  char name[NAME_MAX];
+  unsigned size;
+  unsigned char type;
+} file_info;
 
 struct prog_options* options;
 
 int entry_cmp(void const* p1, void const* p2)
 {
-  char* left = *((char**) p1);
-  char* right = *((char**) p2);
+  file_info left = *((file_info*) p1);
+  file_info right = *((file_info*) p2);
 
-  return strncmp(left, right, NAME_MAX);
+  return strncmp(left.name, right.name, NAME_MAX);
 }
 int entry_cmp_r(void const* p1, void const* p2)
 {
-  char* left = *((char**) p1);
-  char* right = *((char**) p2);
+  file_info left = *((file_info*) p1);
+  file_info right = *((file_info*) p2);
 
-  return -strncmp(left, right, NAME_MAX);
+  return -strncmp(left.name, right.name, NAME_MAX);
 }
 
 void usage(FILE* fp)  // Print usage/help for the programm
@@ -54,12 +68,105 @@ void usage(FILE* fp)  // Print usage/help for the programm
   fprintf(fp, "\t-r\tReverse the sorting of the list\n");
 }
 
+// TODO: Find out how to get all the colors (like gray,brown etc.)
+char* ls_to_ansi_color(char ls, int fg)
+{
+  switch (ls)
+  {
+    case 'a':
+      if (fg)
+        return BLK;
+      else
+        return BLKB;
+    case 'b':
+      if (fg)
+        return RED;
+      else
+        return REDB;
+    case 'c':
+      if (fg)
+        return GRN;
+      else
+        return GRNB;
+    case 'd':
+      if (fg)
+        return HGRN;
+      else
+        return GRNHB;
+    case 'e':
+      if (fg)
+        return BLU;
+      else
+        return BLUB;
+    case 'f':
+      if (fg)
+        return MAG;
+      else
+        return MAGB;
+    case 'g':
+      if (fg)
+        return CYN;
+      else
+        return CYNB;
+    case 'h':
+      if (fg)
+        return HGRN;
+      else
+        return GRNHB;
+    case 'A':
+      if (fg)
+        return BBLK;
+      else
+        return BLKHB;
+    case 'B':
+      if (fg)
+        return BRED;
+      else
+        return REDHB;
+    case 'C':
+      if (fg)
+        return BGRN;
+      else
+        return GRNHB;
+    case 'D':
+      if (fg)
+        return BHGRN;
+      else
+        return GRNHB;
+    case 'E':
+      if (fg)
+        return BBLU;
+      else
+        return BLUHB;
+    case 'F':
+      if (fg)
+        return BMAG;
+      else
+        return MAGB;
+    case 'G':
+      if (fg)
+        return BCYN;
+      else
+        return CYNB;
+    case 'H':
+      if (fg)
+        return BHGRN;
+      else
+        return GRNHB;
+    case 'x':
+      return "";
+    default:
+      return "";
+  }
+}
+
 int main(const int argc, char* const argv[])
 {
   // parse options
   options = malloc(sizeof(*options));
   options->order_rev = 0;
   options->show_hidden = 0;
+  options->print_color = 0;
 
   if (argc == 1)
   {
@@ -74,7 +181,7 @@ int main(const int argc, char* const argv[])
     int c;
     opterr = 0;
 
-    while ((c = getopt(argc, argv, "rah")) != -1)
+    while ((c = getopt(argc, argv, "rahc")) != -1)
     {
       switch (c)
       {
@@ -147,10 +254,7 @@ int main(const int argc, char* const argv[])
   DIR* dir_p;
   struct dirent* entry_p;
 
-  char** entries = malloc(MAX_LIST_SIZE * sizeof(char*));
-  for (int i = 0; i < MAX_LIST_SIZE; i++)
-    entries[i] = malloc(NAME_MAX * sizeof(char));
-
+  file_info* entries = calloc(MAX_LIST_SIZE, sizeof(*entries));
   int entry_idx = 0;
 
   dir_p = opendir(options->directory);
@@ -159,8 +263,10 @@ int main(const int argc, char* const argv[])
     while ((entry_p = readdir(dir_p)))
     {
       // entry_p->d_name should always be shorter then (linux limits)
+      entries[entry_idx].type = entry_p->d_type;
+      entries[entry_idx].size = entry_p->d_reclen;
 
-      strncpy(entries[entry_idx], entry_p->d_name, NAME_MAX);
+      strncpy(entries[entry_idx].name, entry_p->d_name, NAME_MAX);
       ++entry_idx;
     }
   } else
@@ -175,32 +281,127 @@ int main(const int argc, char* const argv[])
 
   // sort entries
   if (options->order_rev)
-    qsort(entries, entry_idx, sizeof(char*), entry_cmp_r);
+    qsort(entries, entry_idx, sizeof(*entries), entry_cmp_r);
   else
-    qsort(entries, entry_idx, sizeof(char*), entry_cmp);
+    qsort(entries, entry_idx, sizeof(*entries), entry_cmp);
+
+  // check for LSCOLORS
+  int print_colors = 1;
+
+  char** colors = malloc(CATEGORIES * sizeof(*colors));
+  for (int i = 0; i < CATEGORIES; i++)
+    colors[i] = malloc(COLOR_CODE_LENGTH * sizeof(**colors));
+
+  const char* colors_config = getenv("LSCOLORS");
+  if (!colors_config)
+  {
+    print_colors = 0;
+  } else
+  {
+    if (strnlen(colors_config, 12) != 12)
+    {
+      fprintf(stderr, "ERROR: Failed trying to parse $LSCOLORS\n");
+      closedir(dir_p);
+      free(entries);
+      free(options);
+      free(buff);
+      return EXIT_FAILURE;
+    }
+
+    // Parsing all the color codes, order stems from the man pages for readdir()
+    //
+    // Color for block devices
+    for (int i = 0; i < CATEGORIES; i++)
+    {
+      char fg = colors_config[i * 2];
+      char bg = colors_config[i * 2 + 1];
+
+      strncat(colors[i], ls_to_ansi_color(fg, 1), 20);
+      strncat(colors[i], ls_to_ansi_color(bg, 0), 20);
+    }
+  }
 
   for (int i = 0; i < entry_idx; i++)
   {
     // assemble buff
-    if (entries[i][0] != '.' || options->show_hidden)
+    if (entries[i].name[0] != '.' || options->show_hidden)
     {
-      int l = strnlen(entries[i], NAME_MAX);
+      int l = strnlen(entries[i].name, NAME_MAX);
       if (l + idx >= b_len)  // check for buff size
       {
         buff = realloc(buff, b_len * 2);
         b_len *= 2;
       }
-      strncpy(&buff[idx], entries[i], l);
+
+      if (print_colors)
+      {
+        char* color_code = malloc(20 * sizeof(*color_code));
+
+        // TODO: make this proper, and add all the colors
+        switch (entries[i].type)
+        {
+          case DT_REG: {
+            struct stat sb;
+            if (stat(entries[i].name, &sb) == 0 &&
+                sb.st_mode & S_IXUSR)  // is executable
+              strncpy(color_code, colors[4], 20);
+            else
+              strncpy(color_code, "", 20);
+            break;
+          }
+          case DT_DIR:
+            strncpy(color_code, colors[0], 20);
+            break;
+          case DT_LNK:
+            strncpy(color_code, colors[1], 20);
+            break;
+          case DT_SOCK:
+            strncpy(color_code, colors[2], 20);
+            break;
+          case DT_FIFO:
+            strncpy(color_code, colors[3], 20);
+            break;
+          case DT_BLK:
+            strncpy(color_code, colors[5], 20);
+            break;
+          case DT_CHR:
+            strncpy(color_code, colors[6], 20);
+            break;
+        }
+
+        int cl_l = strnlen(color_code, 20);
+
+        if (cl_l + idx >= b_len)
+        {
+          buff = realloc(buff, b_len * 2);
+          b_len *= 2;
+        }
+
+        strncpy(&buff[idx], color_code, cl_l);
+        idx += cl_l;
+      }
+
+      strncpy(&buff[idx], entries[i].name, l);
+
       idx += l;
       buff[idx] = '\t';
       ++idx;
+
+      // put in reset code
+      int rs_l = strnlen(reset, 10);
+
+      if (rs_l + idx >= b_len)
+      {
+        buff = realloc(buff, b_len * 2);
+        b_len *= 2;
+      }
+
+      strncpy(&buff[idx],reset,rs_l);
     }
   }
   puts(buff);
 
   closedir(dir_p);
-
-  for (int i = 0; i < MAX_LIST_SIZE; i++) free(entries[i]);
   free(entries);
 
   free(options);
